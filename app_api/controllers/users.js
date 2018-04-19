4d052fc6436b46a17c643d613a0b3ce795e219af
Gustavo Bejarano <gbejaran@mymail.mines.edu>
2018-04-18 20:35:47 -0600

    Added api calls for userById, listPostings, and postingById

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -1,1 +1,22 @@
-module.exports.userById = function (req, res) {};
+module.exports.userById = function (req, res) {
+  if (req.params && req.params.userid) {
+    User
+      .findById(req.params.userid)
+      .exec(function(err, user) {
+        if (!user) {
+          sendJsonResponse(res, 404, {
+            "message": "courseid not found"
+          });
+          return;
+        } else if (err) {
+          sendJsonResponse(res, 404, err);
+          return;
+        }
+        sendJsonResponse(res, 200, user);
+      });
+  } else {
+    sendJsonResponse(res, 404, {
+      "message": "No courseid in request"
+    });
+  }
+};
