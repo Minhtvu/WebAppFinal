6c31b3880007bb2c238240ced17cb5c778d2fab2
gbejaran <gbejaran@mymail.mines.edu>
2018-04-22 15:29:20 -0600

    Started login features

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 var router = express.Router();
 var ctrlPostings = require('../controllers/postings');
+var config = require('../controllers/config');
+var funct = require('../controllers/functions');
+
+var app = express();
+app.use(session({secret: 'supernova', savedUninitialized: true, resave:true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* GET postings pages. */
 router.get('/', ctrlPostings.postList);
